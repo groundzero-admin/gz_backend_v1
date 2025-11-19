890b77fbd4647caff0578282348f1d601852f240
adarsh-iiit <adarshtech251@gmail.com>
2025-11-19 13:21:27 +0530

    testing of localdev branch

--- loadenv.js
+++ loadenv.js
@@ -3,3 +3,8 @@ import dotenv from "dotenv";
 // Load environment variables FIRST
 dotenv.config();
 console.log("✅ .env loaded" , process.env.PORT);
+
+
+
+
+//// this is just a comment to check is hte local devbranch created or not , iyes is the code is going in that branch only or not 
